b698c037c132433112b6ba8f23d982892c02155c
egerambo <ege_telatar@hotmail.com>
2019-09-24 12:53:15 -0600

    Finished last part

--- lab3_script.sh
+++ lab3_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# Authors : Your Name & Your partner's name
-# Date: 1/1/2019
+# Authors : Ege Telatar
+# Date: 09/24/2019
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
@@ -8,6 +8,7 @@
 echo "enter expression"
 read regex_input filename
 
-grep $regex_input $filename
+#grep $regex_input $filename
 grep -c -P '[0-9]{3}-[0-9]{3}-[0-9]{4}' regex_practice.txt
 grep -c -P '303-[0-9]{3}-[0-9]{4}'  regex_practice.txt
+grep -c -P '@geocities.com'   regex_practice.txt &> email_results.txt
